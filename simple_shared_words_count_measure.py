f3d9c65e02193872b4a846529bcd963760d42cb9
robertoravec141 <roravec141@gmail.com>
2020-04-05 13:26:19 +0200

    Working on vectors

--- simple_shared_words_count_measure.py
+++ simple_shared_words_count_measure.py
@@ -2,20 +2,46 @@ from common import *
 
 file_name = 'test.txt'
 
+class QueryToken:
+    def __init__(self, token):
+        self.token = token
+        self.hits = 0
+
+class Vector:
+    def __init__(self, query):
+        self.queryTokens = map(lambda token: QueryToken(token), query)
+
+    def add_hit(self, token):
+        for queryToken in self.queryTokens:
+            if queryToken.token == token:
+                queryToken.hits += 1
+
+    # Returns vector as array of int
+    def get_vector_value(self):
+        return map(lambda queryToken: queryToken.hits, self.queryTokens)
+
+class DocumentVectorizer:
+    def __init__(self, document):
+        self.document = document
+
+    def generate_vector(self, query):
+        document_tokens = tokenize(self.document)
+        vector = Vector(query)
+        for token in document_tokens:
+            vector.add_hit(token)
+        return vector.get_vector_value
+
+
 search = "more"
-documents = create_documents_from_file(file_name)
-searchTokens = tokenize(search)
-results = []
-for document in documents:
-    documentTokens = tokenize(document)
-    hits = 0
-    for searchToken in searchTokens:
-        if searchToken in documentTokens:
-            hits += 1
-    if hits > 0:
-        result = SharedWorldCountResult(document, hits)
-        results.append(result)
-results.sort(key = lambda result: result.resultCount, reverse = True)
+query = "more"
+
 print('🔥 First task 🔥\n')
-for result in results[:10]:
-    print('Hits: ' + str(result.resultCount) + '\nDocument: ' + result.document + '\n')
+
+documents = create_documents_from_file(file_name)
+query = tokenize(search)
+for document in documents:
+    vectorizer = DocumentVectorizer(document)
+    vector = vectorizer.generate_vector(query)
+    print(vector)
+
+
